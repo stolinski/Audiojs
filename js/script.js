a6be507581532e361577f3d28df2c96308674376
Scott Tolinski <scotttolinski@gmail.com>
2012-01-18 16:33:02 -0500

    working audio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
 /* Author: 
 
 */
+$(function() {
+	$( "#slider-vertical" ).slider({
+		orientation: "vertical",
+		range: "min",
+		min: 220,
+		max: 880,
+		value: 440,
+		slide: function( event, ui ) {
+			$( "#amount" ).val( ui.value );
+		}
+	});
+	$( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+var no = $( "#slider-vertical" ).slider( "value" );
+console.log(no);
 
+});
 
+function playExample() {
+	var notez = document.getElementById('amount').value;
+console.log(notez);
+    var AudioletApp = function() {
+        this.audiolet = new Audiolet();
+        this.sine = new Sine(this.audiolet, 440);
+        this.modulator = new Saw(this.audiolet, notez);
+        this.modulatorMulAdd = new MulAdd(this.audiolet, 200, 440);
 
+        this.modulator.connect(this.modulatorMulAdd);
+        this.modulatorMulAdd.connect(this.sine);
+        this.sine.connect(this.audiolet.output);
+    };
 
-
-
-
-
-
-
-
+    this.audioletApp = new AudioletApp();
+};
 
 
 
